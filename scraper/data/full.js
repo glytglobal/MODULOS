0740033cceeb3aefe1e2b0ddc6892898b5512ce4
GL YT MX <148560941+glytglobal@users.noreply.github.com>
2024-10-04 18:25:58 -0300

    ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ

---
+++ scraper/data/full.js
@@ -0,0 +1,16 @@
+// <!> IMPORTACION 
+
+// - ARCHIVO
+// import (funcion) from '(scraper)';
+// • EJEMPLO
+// import { search, download } from 'aptoide-scraper';
+
+// - PACKAGES
+// "(nombre)": "github:glytglobal/modulos-sft",
+// • EJEMPLO 
+// "aptoide-scraper": "github:glytglobal/modulos-sft",
+
+// <!> NOTA
+// PUEDE UTILIZAR ESTOS COMPLEMENTOS (SCRAPER'S)
+// ESTO DEBE ESTAR ANTERIORMENTE EN PACKAGE.JSON(*)
+// PARA EVITAR ERRORES O TENGAS PROBLEMAS
